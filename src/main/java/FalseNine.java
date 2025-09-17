550f3ea061e2bd90231262b8c1d7e46b91a625bc
Suyog Nimbre <nsuyog@yahoo.com>
2025-09-17 20:33:30 +0530

    Collections

--- src/main/java/FalseNine.java
+++ src/main/java/FalseNine.java
@@ -28,33 +28,33 @@ public class FalseNine {
     }
 
     public static void main(String[] args) {
-        List<FalseNine> students = new ArrayList<>();
-        students.add(new FalseNine(521,"Thomas Muller",75));
-        students.add(new FalseNine(522,"Lionel Messi",100));
-        students.add(new FalseNine(523,"Roberto Firmino",75));
-        students.add(new FalseNine(524,"Franceso Totti",85));
-        students.add(new FalseNine(525,"Dennis Bergkmap",90));
+        List<FalseNine> falsenine = new ArrayList<>();
+        falsenine.add(new FalseNine(521,"Thomas Muller",75));
+        falsenine.add(new FalseNine(522,"Lionel Messi",100));
+        falsenine.add(new FalseNine(523,"Roberto Firmino",75));
+        falsenine.add(new FalseNine(524,"Franceso Totti",85));
+        falsenine.add(new FalseNine(525,"Dennis Bergkmap",90));
 
-        System.out.println("Students in List");
+        System.out.println("False 9s in List");
 
-        for(FalseNine s:students){
+        for(FalseNine s:falsenine){
             System.out.println(s);
         }
         System.out.println();
 
-        Map<Integer,String> studentMap = new HashMap<>();
-        for(FalseNine s:students){
-            studentMap.put(s.getId(),s.getName());
+        Map<Integer,String> falseMap = new HashMap<>();
+        for(FalseNine s:falsenine){
+            falseMap.put(s.getId(),s.getName());
         }
 
-        System.out.println("Map of Student IDs to Names:");
-        for (Map.Entry<Integer, String> entry : studentMap.entrySet()) {
+        System.out.println("Map of False 9s IDs to Names:");
+        for (Map.Entry<Integer, String> entry : falseMap.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
         System.out.println();
 
         Set<Integer> uniqueMarks = new HashSet<>();
-        for(FalseNine s:students){
+        for(FalseNine s:falsenine){
             uniqueMarks.add(s.getMarks());
         }
 
